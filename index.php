9dacab1538413119eadf8d02c9758331f5c1af13
tenorfelix <125461795+tenorfelix@users.noreply.github.com>
2025-10-14 12:48:22 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php get_header(); ?>
+
+<main>
+  <?php
+  if ( have_posts() ) :
+    while ( have_posts() ) : the_post();
+      the_title('<h1>','</h1>');
+      the_content();
+    endwhile;
+  else :
+    echo '<p>Keine Inhalte gefunden.</p>';
+  endif;
+  ?>
+</main>
+
+<?php get_footer(); ?>
